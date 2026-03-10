e94c9e13d03211766b4edb52ceac3907c4eff43a
janarthanan <janarthanan0012@gmail.com>
2026-03-10 11:50:59 +0530

    Remove stats section from landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -180,7 +180,6 @@ export default function Landing() {
                             </button>
                         </div>
                     </motion.div>
-
                 </div>
             </section>
 
